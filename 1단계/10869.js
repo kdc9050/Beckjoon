752b439ae9bbc19dc760f95627d26418d0af8ade
kdc <kdc9050@naver.com>
2023-09-10 18:32:35 +0900

    two

---
+++ 1단계/10869.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout});
+
+rl.on('line', line => {
+ let arr = line.split(" ");
+ let a = parseInt(arr[0]);
+ let b = parseInt(arr[1]);
+ console.log(a+b);
+ console.log(a-b);
+ console.log(a*b);
+ console.log(Math.floor(a / b));
+ console.log(a%b);
+})
